add4c6561ed59579c9d5832167c6d981448b95c6
shaneorourke <shaneorourke110@gmail.com>
2022-03-18 12:58:26 +0000

    Add files via upload

--- db_queries_adhoc.py
+++ db_queries_adhoc.py
@@ -1,17 +1,7 @@
 import sqlite3 as sql
-from datetime import datetime
-from binance import Client
-import binance_keys as bk
-from rich.console import Console
-from rich.theme import Theme
 
 conn = sql.connect('crypto_trading.db')
 c = conn.cursor()
-
-customer_theme = Theme({'info':"bold green italic",'integer':'blue bold','pos_warning':'yellow bold italic','neg_warning':'red bold'})
-console = Console(color_system='auto',theme=customer_theme)
-
-client = Client(api_key=bk.API_KEY,api_secret=bk.SECRET_KEY)
 
 replace = ['(',')',',','./data/','csv','.','[',']']
 replace_number = ['(',')',',','[',']']
@@ -33,4 +23,3 @@ result = c.fetchall()
 result = clean_up_sql_out(result,1)
 print(result)
 
-console.print()
